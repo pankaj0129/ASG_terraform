4246c12c96ceebab3738d2ecca1122e0bc367e83
pankaj0129 <133801920+pankaj0129@users.noreply.github.com>
2023-12-19 00:17:02 +0530

    Update checkout.groovy

--- src/vars/checkout.groovy
+++ src/vars/checkout.groovy
@@ -1,12 +1,10 @@
-// File: src/vars/checkout.groovy
-
 def call() {
-    script {
-        def scm = [
-            branch: 'root_module',
-            url: 'https://github.com/pankaj0129/ASG_terraform.git'
-        ]
-        checkout scm
+    stage('checkout') {
+        steps {
+            script {
+                checkout([$class: 'GitSCM', branches: [[name: 'root_module']], userRemoteConfigs: [[url: 'https://github.com/pankaj0129/ASG_terraform.git']]])
+            }
+        }
     }
 }
 
