eeaef962a69a8decf816af988f0ad65d7a3fd223
pankaj0129 <133801920+pankaj0129@users.noreply.github.com>
2023-12-19 00:11:28 +0530

    Update infra-cost.groovy

--- src/vars/infra-cost.groovy
+++ src/vars/infra-cost.groovy
@@ -1,7 +1,7 @@
 // File: src/vars/infra-cost.groovy
 
 def call() {
-    stage('Infra Cost') {
+    stage('infra-Cost') {
         steps {
             withCredentials([string(credentialsId: 'INFRACOST_API_KEY', variable: 'INFRACOST_API_KEY')]) {
                 sh 'infracost breakdown --path . --format html > infracost_report.html'
